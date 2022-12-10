28b8eadd6e03bb0888025d1765198cc635369782
Mikhail-Kavzov <kavzovm@mail.ru>
2022-12-10 16:31:55 +0300

    add singleton test

--- Container.Tests/ContainerTests.cs
+++ Container.Tests/ContainerTests.cs
@@ -1,18 +1,114 @@
+using DIContainer;
+using DIContainer.Enum;
+using DIContainer.Examples;
+using DIContainer.Implementation;
+using DIContainer.Interfaces;
 using NUnit.Framework;
 
 namespace Container.Tests
 {
     public class ContainerTests
     {
+        private  IDependencyProvider _provider;
+
         [SetUp]
         public void Setup()
         {
+            var configuration = new DependencyConfiguration();
+            configuration.Register<IImplementation,DefaultConstructorClass>();
+            configuration.Register<IService, ServiceImpl>();
+            configuration.Register<ISomeService, SomeServiceImpl>();
+            configuration.Register<IRepository, RepositoryImpl>();
+            configuration.Register<IService<IRepository>, ServiceImpl<IRepository>>();
+
+            _provider = new DependencyProvider(configuration);
+
         }
 
+        /// <summary>
+        /// Recursion generic test
+        /// </summary>
         [Test]
-        public void Test1()
+        public void RecursionGenericTest()
         {
-            Assert.Pass();
+            var result=_provider.Resolve<IService<IRepository>>();
+            Assert.That(result.GetType() == typeof(ServiceImpl<IRepository>));
+            ServiceImpl<IRepository> service = (ServiceImpl<IRepository>)result;
+            Assert.That(service.rep.GetType() == typeof(RepositoryImpl));
         }
+
+        /// <summary>
+        /// Singleton example
+        /// </summary>
+        [Test]
+        public void SingletonTest()
+        {
+            var configuration = new DependencyConfiguration();
+            configuration.Register<IImplementation, DefaultConstructorClass>(LifeTime.Singleton);
+            var provider = new DependencyProvider(configuration);
+            var result1 = provider.Resolve<IImplementation>();
+            var result2 = provider.Resolve<IImplementation>();
+            Assert.That(result1.Equals(result2)); // links are equal
+        }
+
+        /// <summary>
+        /// Interface -> implementation Test
+        /// </summary>
+        [Test]
+        public void InterfaceImplementationTest()
+        {
+            var configuration = new DependencyConfiguration();
+            configuration.Register<IImplementation, DefaultConstructorClass>();
+            var provider = new DependencyProvider(configuration);
+            var result = provider.Resolve<IImplementation>();
+            Assert.IsNotNull(result);
+            Assert.That(result.GetType()==typeof(DefaultConstructorClass));
+        }
+
+        /// <summary>
+        /// Implementation contains interface - throws exception
+        /// </summary>
+        [Test]
+        public void InterfaceInImplementationTest()
+        {
+            var configuration = new DependencyConfiguration();
+            configuration.Register<IImplementation, Impl>();
+            Assert.Throws<ArgumentException>(() => new DependencyProvider(configuration));
+        }
+
+        /// <summary>
+        /// Implementation contains abstract class - throws exception
+        /// </summary>
+        [Test]
+        public void AbstractClassInImplementationTest()
+        {
+            var configuration = new DependencyConfiguration();
+            configuration.Register<Impl, AbstractClass>();
+            Assert.Throws<ArgumentException>(() => new DependencyProvider(configuration));
+        }
+
+        /// <summary>
+        /// Configuration contains the same implementations (Dependency -> implementation)
+        /// LifeTime doesn't matter
+        /// </summary>
+        [Test]
+        public void SameImplementationsTest()
+        {
+            var configuration = new DependencyConfiguration();
+            configuration.Register<IImplementation, DefaultConstructorClass>();
+            configuration.Register<IImplementation, DefaultConstructorClass>();
+            Assert.Throws<ArgumentException>(() => new DependencyProvider(configuration));
+        }
+
+        /// <summary>
+        /// Configuration is null
+        /// </summary>
+        [Test]
+        public void NullConfigurationTest()
+        {
+            Assert.Throws<ArgumentNullException>(() => new DependencyProvider(null));
+        }
+
+
     }
 }
---
+++ Container.Tests/Examples/TestClasses.cs
@@ -0,0 +1,90 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DIContainer.Examples
+{
+    interface Impl : IImplementation
+    {
+        void Method2();
+    }
+
+    interface IImplementation
+    {
+        void Method();
+    }
+
+    class DefaultConstructorClass : IImplementation
+    {
+        public DefaultConstructorClass()
+        {
+        }
+
+        public void Method()
+        {
+        }
+    }
+
+    abstract class AbstractClass : Impl
+    {
+
+        public void Method()
+        {
+
+        }
+
+        public void Method2()
+        {
+
+        }
+    }
+
+    interface IRepository
+    {
+
+    }
+
+    interface IService<TRepository> where TRepository : IRepository
+    {
+    }
+
+    class ServiceImpl<TRepository> : IService<TRepository>
+        where TRepository : IRepository
+    {
+        public TRepository rep;
+
+        public ServiceImpl(TRepository repository)
+        {
+            rep = repository;
+        }
+
+    }
+
+
+    interface IService { }
+    class ServiceImpl : IService
+    {
+        public IRepository _rep;
+
+        public ServiceImpl(IRepository repository) 
+        {
+            _rep=repository;
+        }
+    }
+
+
+    interface ISomeService { }
+    class RepositoryImpl : IRepository
+    {
+        public ISomeService _rep;
+
+        public RepositoryImpl(ISomeService service)
+        {
+            _rep=service;
+        } 
+    }
+
+    class SomeServiceImpl : ISomeService { }
+}
